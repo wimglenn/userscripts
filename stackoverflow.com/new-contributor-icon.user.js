bba2dec5947a196e0b464115c35caf577d8e0bfe
wim glenn <hey@wimglenn.com>
2021-09-27 23:21:12 -0500

    also replace in the answer box banner

--- stackoverflow.com/new-contributor-icon.user.js
+++ stackoverflow.com/new-contributor-icon.user.js
@@ -2,7 +2,7 @@
 // @name         New Contributor
 // @namespace    https://github.com/wimglenn/userscripts/
 // @version      0.1
-// @description  Changes "👋 New contributor" into "💩 New contributor"
+// @description  Changes the emoji on 'New contributor' labels from WAVING HAND SIGN into PILE OF POO
 // @author       wim: https://stackoverflow.com/users/674039/wim
 // @include      /^https?:\/\/\w*.?(stackexchange.com|stackoverflow.com|serverfault.com|superuser.com|askubuntu.com|stackapps.com|mathoverflow.net)\/questions/.*
 // @grant        none
@@ -10,10 +10,10 @@
 
 function changeNewContributorIcons() {
     "use strict";
-    var bs = document.getElementsByClassName("js-new-contributor-label")
+    var bs = document.getElementsByClassName("svg-icon iconWave")
     for (var i = bs.length - 1; i >= 0; --i) {
         const newItem = document.createElement("span")
-        newItem.innerHTML = "💩\xa0 New contributor"
+        newItem.innerHTML = "💩\xa0"
         bs[i].parentNode.replaceChild(newItem, bs[i]);
     }
 };
