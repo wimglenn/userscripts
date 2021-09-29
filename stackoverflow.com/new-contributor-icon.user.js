14d9f7755a090081778136ca315eddee439282b4
wim glenn <hey@wimglenn.com>
2021-09-28 22:23:36 -0500

    MOAR JQUERY

--- stackoverflow.com/new-contributor-icon.user.js
+++ stackoverflow.com/new-contributor-icon.user.js
@@ -1,23 +1,11 @@
 // ==UserScript==
 // @name         New Contributor
 // @namespace    https://github.com/wimglenn/userscripts/
-// @version      0.1
+// @version      0.2
 // @description  Changes the emoji on 'New contributor' labels from WAVING HAND SIGN into PILE OF POO
 // @author       wim: https://stackoverflow.com/users/674039/wim
 // @include      /^https?:\/\/\w*.?(stackexchange.com|stackoverflow.com|serverfault.com|superuser.com|askubuntu.com|stackapps.com|mathoverflow.net)\/questions/.*
 // @grant        none
 // ==/UserScript==
 
-function changeNewContributorIcons() {
-    "use strict";
-    var bs = document.getElementsByClassName("svg-icon iconWave")
-    for (var i = bs.length - 1; i >= 0; --i) {
-        const newItem = document.createElement("span")
-        newItem.innerHTML = "💩\xa0"
-        bs[i].parentNode.replaceChild(newItem, bs[i]);
-    }
-};
-
-$(document).ready(function() {
-  changeNewContributorIcons();
-});
+$(".svg-icon.iconWave").replaceWith("💩\xa0")
--- stackoverflow.com/social-notworking.user.js
+++ stackoverflow.com/social-notworking.user.js
@@ -1,21 +1,11 @@
 // ==UserScript==
 // @name         Social Notworking
 // @namespace    https://github.com/wimglenn/userscripts/
-// @version      0.1
+// @version      0.2
 // @description  Hides the "Know someone who can answer? Share a link to this question via email, Google+, Twitter, or Facebook." spam from questions
 // @author       wim: https://stackoverflow.com/users/674039/wim
 // @include      /^https?:\/\/\w*.?(stackexchange.com|stackoverflow.com|serverfault.com|superuser.com|askubuntu.com|stackapps.com|mathoverflow.net)\/questions/.*
 // @grant        none
 // ==/UserScript==
 
-function removeSocialNetworkingCrap() {
-    "use strict";
-    var bs = document.getElementsByClassName("bottom-notice bottom-share-links")
-    for (var i = bs.length - 1; i >= 0; --i) {
-        bs[i].remove();
-    }
-};
-
-$(document).ready(function() {
-  removeSocialNetworkingCrap();
-});
+$(".bottom-notice.bottom-share-links").remove()
