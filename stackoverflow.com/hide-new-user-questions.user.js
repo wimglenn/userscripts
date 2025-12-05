3c9190037d28c339bb2a95270c9368c661750c65
Wim Jeantine-Glenn <hey@wimglenn.com>
2025-12-05 00:00:15 -0600

    hide crap

---
+++ stackoverflow.com/hide-new-user-questions.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Hide rep 1 user questions
+// @namespace    https://github.com/wimglenn/userscripts/
+// @version      0.1
+// @description  Improve signal-to-noise ratio of the question feed on stackoverflow.com
+// @author       wim: https://stackoverflow.com/users/674039/wim
+// @match        stackoverflow.com/questions*
+// @grant        none
+// @updateURL    https://github.com/wimglenn/userscripts/raw/refs/heads/main/adventofcode.com/hide-new-user-questions.user.js
+// @downloadURL  https://github.com/wimglenn/userscripts/raw/refs/heads/main/adventofcode.com/hide-new-user-questions.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    function hide() {
+        var things = document.getElementsByClassName("todo-no-class-here");
+        for (var i = 0; i < things.length; i++) {
+            var t = things[i]
+            if (t.title === "reputation score " && t.textContent === "1") {
+                var listEl = t.closest('div[itemprop="itemListElement"]')
+                listEl.remove()
+            }
+        }
+    }
+    hide()
+    const observer = new MutationObserver(() => hide());
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true,
+    });
+})();
