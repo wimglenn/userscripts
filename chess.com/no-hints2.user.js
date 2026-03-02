c18b3255039f138459dcff9a3bb7c86708b1e7cd
Wim Jeantine-Glenn <hey@wimglenn.com>
2026-03-02 12:56:46 -0600

    mutation observer

--- chess.com/no-hints2.user.js
+++ chess.com/no-hints2.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         Hide Hint Button 2
 // @namespace    https://github.com/wimglenn/userscripts/
-// @version      0.1
+// @version      0.2
 // @description  Hides the "Hint" button 2
 // @include      https://www.chess.com/puzzles/rated
+// @include      https://www.chess.com/daily
 // @run-at       document-start
 // @grant        none
 // @updateURL    https://github.com/wimglenn/userscripts/raw/refs/heads/main/chess.com/no-hints2.user.js
@@ -11,14 +12,22 @@
 // ==/UserScript==
 
 (function () {
-  'use strict';
-
-  const style = document.createElement('style');
-  style.textContent = `
-    button[data-cy="hint-move-button"] {
-      display: none !important;
+  const addStyle = () => {
+    if (!document.getElementById('hide-hint-style')) {
+      const style = document.createElement('style');
+      style.id = 'hide-hint-style';
+      style.textContent = `
+        button[data-cy="hint-move-button"] {
+          display: none !important;
+        }
+      `;
+      document.documentElement.appendChild(style);
     }
-  `;
+  };
 
-  document.documentElement.appendChild(style);
+  addStyle();
+  new MutationObserver(addStyle).observe(document.documentElement, {
+    childList: true,
+    subtree: true
+  });
 })();
